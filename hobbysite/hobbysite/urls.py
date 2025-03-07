9c4a7ba2a1215dea0056ab31ef4e194aedcc1499
Jan Llenzl Dagohoy <jan.dagohoy@student.ateneo.edu>
2025-03-07 23:37:49 +0800

    fix: update urls.py

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -5,7 +5,7 @@ from wiki import views as wiki_views
 urlpatterns = [
     path('merchstore/', include('merchstore.urls', namespace='merchstore')), 
     path('wiki/', include('wiki.urls', namespace="wiki")),
-    path('', wiki_views.home, name='home'),
+    #path('', wiki_views.home, name='home'),
     path('blog/', include('blog.urls', namespace="blog")),
     path('forum/', include('forum.urls', namespace="forum")),
     path('commissions/', include('commissions.urls', namespace="commissions")),
