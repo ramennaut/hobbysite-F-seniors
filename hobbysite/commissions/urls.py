dac46f39e2e35132e851064a7c3f6889e161418d
Jan Llenzl Dagohoy <jan.dagohoy@student.ateneo.edu>
2025-03-07 20:55:18 +0800

    fix: add commissions

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from .views import index
-from .views import commissions_list
-from .views import commissions_detail_1
+from .views import commision_list, commission_detail
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('list/', commissions_list, name='commissions_list'),
-    path('detail/1', commissions_detail_1, name='commissions_detail_1'),
+    path('list/', commision_list.as_view(), name='commissions_list'),
+    path('detail/<int:pk>/',commission_detail.as_view(), name='commissions_detail'),
 ]
 
 app_name = 'commissions'
