d7cb70e00a00ef006580e72e22d85ba39cfb4051
Kathleene Soto <kat.csoto@gmail.com>
2025-04-16 00:44:48 +0800

    Fix routing issue in blog app

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('articles/', views.list_view, name='list_view'),
-    path('articles/<int:id>/', views.detail_view, name='detail_view'),
-    path('articles/category/<int:id>/', views.category_view, name='category_view')
+    path('article/<int:id>/', views.detail_view, name='detail_view'),
+    path('category/<int:id>/', views.category_view, name='category_view')
 ]
 
 app_name = 'blog'
