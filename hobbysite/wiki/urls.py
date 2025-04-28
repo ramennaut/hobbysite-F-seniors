5a93842e1e894226ded7bfc9be55112c2df2483e
Meik0o0 <miguel.palomares@student.ateneo.edu>
2025-04-28 13:37:16 +0800

    Added final wiki spec changes

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'wiki'
 
 urlpatterns = [
-    path('articles/', views.list_view, name='article_list'),
-    path('article/<int:id>/', views.detail_view, name='article_detail'),
-    path('category/<int:id>/', views.category_view, name='category_list'),
+    path('articles/', views.ArticleListView.as_view(), name='article_list'),
+    path('article/<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
+    path('article/add/', views.ArticleCreateView.as_view(), name='article_create'),
+    path('article/<int:pk>/edit/', views.ArticleUpdateView.as_view(), name='article_update'),
 ]
