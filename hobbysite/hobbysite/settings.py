73f3a05f23c2eee8be9f65d2993714b11f94b420
Jan Llenzl Dagohoy <jan.dagohoy@student.ateneo.edu>
2025-05-02 01:21:27 +0800

    add allowed hosts

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['hobbysite-f-seniors.ondigitalocean.app', 'localhost']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
