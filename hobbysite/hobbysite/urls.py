343c3fceb0fb207be2694a5bc4e36ce6df78ba56
Meik0o0 <miguel.palomares@student.ateneo.edu>
2025-05-01 16:06:29 +0800

    Updated user_management, wiki app, temp update blog app

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.urls import include, path
-from wiki import views as wiki_views
+from django.conf import settings
+from django.conf.urls.static import static
+from user_management.views import HomepageListView
+#from wiki import views as wiki_views
 
 urlpatterns = [
+    path('', HomepageListView.as_view(), name='homepage'),
+    path('admin/', admin.site.urls),
+    path('homepage/', include('user_management.urls')),
     path('merchstore/', include('merchstore.urls', namespace='merchstore')), 
     path('wiki/', include('wiki.urls', namespace="wiki")),
-    #path('', wiki_views.home, name='home'),
     path('blog/', include('blog.urls', namespace="blog")),
     path('forum/', include('forum.urls', namespace="forum")),
     path('commissions/', include('commissions.urls', namespace="commissions")),
-    path('admin/', admin.site.urls),
+    
 ]
